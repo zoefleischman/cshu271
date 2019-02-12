8e68254c69e6917fe5b4c8d254c66eda0101f3cf
JakeBSU <46795426+JakeBSU@users.noreply.github.com>
2019-02-12 16:07:43 -0700

    Update UserAccountManager.java

--- src/business/UserAccountManager.java
+++ src/business/UserAccountManager.java
@@ -3,16 +3,16 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class UserAccountManager {
-	
+
 	public static final String NOINPUTERROR ="";
-	
+
     private ArrayList<UserAccount> userAccounts;
-    
+
     public UserAccountManager() {
         userAccounts = new ArrayList<UserAccount>();
     }
-    
-    public String registerNewUser(String userName, String password, String reenteredPassword, 
+
+    public String registerNewUser(String userName, String password, String reenteredPassword,
     		String firstName, String lastName, String email, String phone){
     		String returnMessage = "";
     		boolean duplicate = false;
@@ -28,8 +28,8 @@ public class UserAccountManager {
 			returnMessage+="Password and reentered password do not match.\n"
 		}
     		returnMessage+=UserAccount.checkInputError(userName, password, firstName, lastName, email, phone);
-    	
-    		// check if userName, password, firstName, lastName, email, or phone is invalid 
+
+    		// check if userName, password, firstName, lastName, email, or phone is invalid
     		// if invalid, return error message
     		// if reenteredPassword does not match password
     		// return an error message;
@@ -44,11 +44,11 @@ public class UserAccountManager {
     		}
     	    return returnMessage;
     }
- 
-    public String updateAccountProfile(UserAccount existingAccount, 
-    		String userName, String password, String reenteredPassword, 
+
+    public String updateAccountProfile(UserAccount existingAccount,
+    		String userName, String password, String reenteredPassword,
     		String firstName, String lastName, String email, String phone){
-		
+
 	    	String returnMessage = "";
     		boolean duplicate = false;
     		for(UserAccount a : userAccounts) {
@@ -63,30 +63,30 @@ public class UserAccountManager {
 			returnMessage+="Password and reentered password do not match.\n"
 		}
     		returnMessage+=UserAccount.checkInputError(userName, password, firstName, lastName, email, phone);
-    	
-	    // check if userName, password, firstName, lastName, email, or phone is invalid 
+
+	    // check if userName, password, firstName, lastName, email, or phone is invalid
 		// if invalid, return error message
 		// if reenteredPassword does not match password
 		// return an error message;
-    		// if there is no profile change (refer to method hasProfileChanges below), return a message 
+    		// if there is no profile change (refer to method hasProfileChanges below), return a message
 		// if userName is changed and the new userName already exists
 		// return an error message;
-	    if(hasProfileChanges(UserAccount existingAccount, String userName, String password, 
+	    if(hasProfileChanges(UserAccount existingAccount, String userName, String password,
     		String firstName, String lastName, String email, String phone)&&returnMessage==""){
-		    
+
   		setAccountProfile(existingAccount, userName, password, firstName, lastName, email, phone);
   		existingAccount.setLastUpdateDate(new Date());
 		    returnMessage=NOINPUTERROR;
 	    }
 	    return NOINPUTERROR;
     }
-    
+
     //TODO
-    // You need to complete this method. 
+    // You need to complete this method.
     // It should be called in method updateAccountProfile
-    private boolean hasProfileChanges(UserAccount existingAccount, String userName, String password, 
+    private boolean hasProfileChanges(UserAccount existingAccount, String userName, String password,
     		String firstName, String lastName, String email, String phone){
-    		// check profile change 
+    		// check profile change
 		return true; // you may change this statement if necessary
     }
 
@@ -98,20 +98,20 @@ public class UserAccountManager {
 		userAccount.setEmail(email);
 		userAccount.setPhoneNumber(phone);
     }
-    
+
     // return the user account if the given userName and password are correct
     // otherwise null
     public UserAccount login(String userName, String password) {
     		for (UserAccount userAccount: userAccounts)
-    			if(userAccount.isValidCredential(userName, password))    
-    				return userAccount;   
+    			if(userAccount.isValidCredential(userName, password))
+    				return userAccount;
     		return null;
     }
-    
+
     public boolean doesUserNameExist(String userName){
     		for (UserAccount userAccount: userAccounts)
-    			if(userAccount.matchUserName(userName))   
-    				return true;   
+    			if(userAccount.matchUserName(userName))
+    				return true;
     		return false;
     }
     //TODO
@@ -122,8 +122,17 @@ public class UserAccountManager {
 		// otherwise check if there is an account that matches the given email
 		// if found, send the user name to the email address
 		// otherwise return an error message
+		if (!UserAccount.isEmailValid(email)) {
+			return "This email either does not exist, or was entered incorrectly. Please try again."
+		}
+		for (UserAccount userAccount: userAccounts) {
+			if (userAccount.matches(email)) {
+				userAccount.sendEmail(EmailMessageType.FORGOT_USER_NAME);
+				return "An email containing your username has been sent."
+			}
+		}
 		return NOINPUTERROR; // you may change this statement if necessary
-   	
+
     }
 
     public String forgotPassword(String userName){
@@ -144,5 +153,5 @@ public class UserAccountManager {
     	}
     }
 
-    
+
 }
