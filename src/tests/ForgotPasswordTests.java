9234e1ac2a312dcb78a8adf5e0a11516e0ef88ee
katiehood <43690729+katierhood@users.noreply.github.com>
2019-02-14 23:40:28 -0700

    added forgotPasswordTests and forgotUserNameTests

--- src/tests/ForgotPasswordTests.java
+++ src/tests/ForgotPasswordTests.java
@@ -1,8 +1,21 @@
 package tests;
 
+import business.UserAccountManager;
 import junit.framework.TestCase;
 
 public class ForgotPasswordTests extends TestCase{
+
+	private UserAccountManager userAccountManager;
 	
+	protected void setUp() throws Exception {
+		super.setUp();
+		userAccountManager = new UserAccountManager();
+		userAccountManager.registerNewUser("jake","j@kepass1","j@kepass1","Jake","Halopoff","jakehalopoff@gmail.com","2087213233");
+	}	
 	
+	public void forgotUserPasswordTest() {
+		assertEquals("That username is invalid. Please try again.", userAccountManager.forgotPassword("bobiscool"));
+		assertEquals("That username does not exist. Please try again.", userAccountManager.forgotPassword("Bob88!!"));
+		assertEquals(UserAccountManager.NOINPUTERROR, userAccountManager.forgotPassword("jake"));			
+	}	
 }
--- src/tests/ForgotUserNameTests.java
+++ src/tests/ForgotUserNameTests.java
@@ -1,8 +1,21 @@
 package tests;
 
+import business.*;
 import junit.framework.TestCase;
 
 public class ForgotUserNameTests extends TestCase{
+
+	private UserAccountManager userAccountManager;
 	
+	protected void setUp() throws Exception {
+		super.setUp();
+		userAccountManager = new UserAccountManager();
+		userAccountManager.registerNewUser("jake","j@kepass1","j@kepass1","Jake","Halopoff","jakehalopoff@gmail.com","2087213233");
+	}	
+		
+	public void forgotUserNameTest() {
+		assertEquals("This email either does not exist, or was entered incorrectly. Please try again.", userAccountManager.forgotUserName("Bob88@gmail.com"));
+		assertEquals(UserAccountManager.NOINPUTERROR, userAccountManager.forgotUserName("jakehalopoff@gmail.com"));			
+	}
 	
 }
